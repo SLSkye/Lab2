535c05030bd212c12347f1c478a0ddd0bc76a37e
Shune Lai Hlaing Hmee <hlainghmees.23@ichat.sp.edu.sg>
2024-04-25 18:57:39 +0800

    final code after min, max and averag calculation

--- Lab2.py
+++ Lab2.py
@@ -10,12 +10,19 @@ def get_user_input():
     userIn = input()
     afterSplit = userIn.split(",")
     ListFloats= list(map(float,afterSplit))
-    print(ListFloats)
     return ListFloats
-
+    
+    
 def calc_average_temperature(ListFloats):
     average = sum(ListFloats)/len(ListFloats)
     return average
+def calc_min_max_temperature(ListFloats):
+    ListFloats.sort()
+    mini = ListFloats[0]
+    maxi = ListFloats[-1]
+    return mini, maxi
+    
+
 
 def main():
     display_main_menu()
@@ -23,8 +30,13 @@ def main():
     print(numberList)
 
     averageTemp = calc_average_temperature(numberList)
-    print(averageTemp)    
+    print("Average is " + str(averageTemp))    
 
+    minTemp, maxTemp = calc_min_max_temperature(numberList)
+    Minimum = int(minTemp)
+    Maximum = int(maxTemp)
+    print("Min temp: " + str(Minimum))
+    print("Max temp: " + str(Maximum))
 
 if __name__ == "__main__":
     main()
