2be10ebe941b8269855276dbb8368b4b44b1a3c3
Shune Lai Hlaing Hmee <hlainghmees.23@ichat.sp.edu.sg>
2024-05-02 14:11:12 +0800

    added median calculation

--- Lab2.py
+++ Lab2.py
@@ -20,6 +20,26 @@ def calc_min_max_temperature(ListFloats):
     mini = ListFloats[0]
     maxi = ListFloats[-1]
     return mini, maxi
+
+def calc_median_temperature(ListFloats):
+    ListFloats.sort()
+    reminder = len(ListFloats)%2
+    if reminder > 0:
+        med = (len(ListFloats)/2) - 0.5
+        medint = int(med)
+        median = ListFloats[medint]
+    else:
+        med1 = (len(ListFloats)/2) -1
+        med2 = (len(ListFloats)/2)  
+        medint1 = int(med1)
+        medint2 = int(med2)
+        m1 = ListFloats[medint1]
+        m2 = ListFloats[medint2]
+
+        median = ((m1+m2)/2)
+
+    return median
+
     
 def main():
     display_main_menu()
@@ -34,6 +54,9 @@ def main():
     Maximum = int(maxTemp)
     print("Min temp: " + str(Minimum))
     print("Max temp: " + str(Maximum))
+
+    calculatedMedian = calc_median_temperature(numberList)
+    print("Median is " + str(calculatedMedian))
 
 if __name__ == "__main__":
     main()
